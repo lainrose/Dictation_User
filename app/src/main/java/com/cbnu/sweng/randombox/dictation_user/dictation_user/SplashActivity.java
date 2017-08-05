21049fb70b576855850249ffd805e58f298af711
lainrose <lainrose@naver.com>
2017-08-05 22:41:22 +0900

    Init

---
+++ app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.cbnu.sweng.randombox.dictation_user.dictation_user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
