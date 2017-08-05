bf93abe9ed93117721479cf4e976353b9f33edf6
lainrose <lainrose@naver.com>
2017-08-05 22:57:00 +0900

    add Splash

--- app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/SplashActivity.java
+++ app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/SplashActivity.java
@@ -1,13 +1,31 @@
 package com.cbnu.sweng.randombox.dictation_user.dictation_user;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cbnu.sweng.randombox.dictation_user.dictation_user.ui.sign.SignInActivity;
+
 public class SplashActivity extends AppCompatActivity {
+
+    static int SPLASH_TIME_OUT = 3000;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(SplashActivity.this, SignInActivity.class);
+                SplashActivity.this.startActivity(loginIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
