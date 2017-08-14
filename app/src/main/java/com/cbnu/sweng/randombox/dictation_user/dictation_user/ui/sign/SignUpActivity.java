800916856a3d19e0b31908b3edb0423b5738755e
lainrose <lainrose@naver.com>
2017-08-14 09:45:40 +0900

    Fill up the contents of dialog in Sign Up Acitivity

--- app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/ui/sign/SignUpActivity.java
+++ app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/ui/sign/SignUpActivity.java
@@ -1,47 +1,54 @@
 package com.cbnu.sweng.randombox.dictation_user.dictation_user.ui.sign;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cbnu.sweng.randombox.dictation_user.dictation_user.R;
+import com.shawnlin.numberpicker.NumberPicker;
 
+import java.lang.reflect.Array;
+
+import butterknife.BindArray;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-//17-08-14
+
 public class SignUpActivity extends AppCompatActivity {
 
+    @BindArray(R.array.strArrayCity)
+    String [] strArrayCity;
     @BindView(R.id.etSchoolNameUp) EditText etSchoolNameUp;
     @BindView(R.id.etStudentInfoUp) EditText etStudentInfoUp;
     @BindView(R.id.btSignUp) Button btSignUp;
+    public Spinner spState;
     @OnClick(R.id.etSchoolNameUp)
     void onClickEtSchoolNameUp(){
-
-    }
-    @OnClick(R.id.etStudentInfoUp)
-    void onClickEtStudentInfoUp(){
-        View mView = getLayoutInflater().inflate(R.layout.dialog_select_student_info, null);
-        final Spinner spGrade = ButterKnife.findById(mView, R.id.spGrade);
-        final Spinner spClass = ButterKnife.findById(mView, R.id.spClass);
-        final Spinner spAttendenceNum = ButterKnife.findById(mView, R.id.spAttendenceNum);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_select_school_name, null);
+        final Spinner spCity = ButterKnife.findById(mView, R.id.spCity);
+        spState = ButterKnife.findById(mView, R.id.spState);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyDialogTheme);
-        builder.setTitle("학년 / 반 / 이름을 선택해주세요.");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setTitle("학교를 검색합니다.");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which){
-                Log.d("TAG", spGrade.getSelectedItem().toString() + spClass.getSelectedItem().toString() + spAttendenceNum.getSelectedItem().toString());
+
             }
         });
-        builder.setNegativeButton("CANCEL",
+        builder.setNegativeButton("취소",
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -50,23 +57,116 @@ public class SignUpActivity extends AppCompatActivity {
                 });
         builder.setView(mView);
 
-        ArrayAdapter<String> GradeAdapter = new ArrayAdapter<String>(SignUpActivity.this,
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<String>(SignUpActivity.this,
                                                                         android.R.layout.simple_spinner_item,
-                                                                        getResources().getStringArray(R.array.strArrayGrade));
-        GradeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spGrade.setAdapter(GradeAdapter);
+                                                                        getResources().getStringArray(R.array.strArrayCity));
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCity.setAdapter(cityAdapter);
+        spCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("tag", spCity.getSelectedItem().toString());
+                Log.d("tag", strArrayCity[0]);
+                ((TextView)adapterView.getChildAt(0)).setTextColor(Color.BLACK);
+                if(spCity.getSelectedItem().toString().equals("시/도")){
+                    spState.setClickable(false);
+                    spState.setSelection(0);
+                }
+                else{
+                    if(spCity.getSelectedItem().toString().equals(strArrayCity[1])){
+                        setStateApdapter(R.array.strArraySeoulState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[2])) {
+                        setStateApdapter(R.array.strArrayBusanState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[3])) {
+                        setStateApdapter(R.array.strArrayDagueState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[4])) {
+                        setStateApdapter(R.array.strArrayIncheonState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[5])) {
+                        setStateApdapter(R.array.strArrayGwangJuState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[6])) {
+                        setStateApdapter(R.array.strArrayDaJeonState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[7])) {
+                        setStateApdapter(R.array.strArrayUlsanState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[8])) {
+                        setStateApdapter(R.array.strArraySeJongState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[9])) {
+                        setStateApdapter(R.array.strArrayGyeonGgiDo);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[10])) {
+                        setStateApdapter(R.array.strArrayGangWonDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[11])) {
+                        setStateApdapter(R.array.strArrayChungCheongBukDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[12])) {
+                        setStateApdapter(R.array.strArrayChungCheongNamDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[13])) {
+                        setStateApdapter(R.array.strArrayJeolLaBukDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[14])) {
+                        setStateApdapter(R.array.strArrayJeolLaNamDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[15])) {
+                        setStateApdapter(R.array.strArrayGyeongSangBukDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[16])) {
+                        setStateApdapter(R.array.strArrayGyeongSangNamDoState);
+                    } else if (spCity.getSelectedItem().toString().equals(strArrayCity[17])) {
+                        setStateApdapter(R.array.strArrayJeJuState);
+                    }
+                    spState.setClickable(true);
+                }
+            }
 
-        ArrayAdapter<String> ClassAdapter = new ArrayAdapter<String>(SignUpActivity.this,
-                                                                        android.R.layout.simple_spinner_item,
-                                                                        getResources().getStringArray(R.array.strArrayClass));
-        ClassAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spClass.setAdapter(ClassAdapter);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //
+            }
+        });
 
-        ArrayAdapter<String> AttendenceNumAdapter = new ArrayAdapter<String>(SignUpActivity.this,
-                                                                                android.R.layout.simple_spinner_item,
-                                                                                getResources().getStringArray(R.array.strArrayAttendenceNum));
-        AttendenceNumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spAttendenceNum.setAdapter(AttendenceNumAdapter);
+        ArrayAdapter<String> StateAdapter = new ArrayAdapter<String>(SignUpActivity.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.strArraySeoulState));
+        StateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spState.setAdapter(StateAdapter);
+        spState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView)adapterView.getChildAt(0)).setTextColor(Color.BLACK);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+    @OnClick(R.id.etStudentInfoUp)
+    void onClickEtStudentInfoUp(){
+        View mView = getLayoutInflater().inflate(R.layout.dialog_select_student_info, null);
+        final NumberPicker npGrade = ButterKnife.findById(mView, R.id.npGrade);
+        final NumberPicker npClass = ButterKnife.findById(mView, R.id.npClass);
+        final NumberPicker npAttendenceNum = ButterKnife.findById(mView, R.id.npAttendenceNum);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyDialogTheme);
+        builder.setTitle("학년 / 반 / 이름을 선택해주세요.");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                int strGrade = npGrade.getValue();
+                int strClass = npClass.getValue();
+                int strAttendenceNum = npAttendenceNum.getValue();
+                etStudentInfoUp.setText(Integer.toString(strGrade) + "학년 "
+                                            + Integer.toString(strClass) + "반 "
+                                            + Integer.toString(strAttendenceNum) + "번 "
+                );
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
+        builder.setView(mView);
 
         AlertDialog dialog = builder.create();
         dialog.show();
@@ -84,4 +184,13 @@ public class SignUpActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
     }
+
+    private void setStateApdapter(int state){
+        ArrayAdapter<String> StateAdapter = new ArrayAdapter<String>(SignUpActivity.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(state));
+        StateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spState.setAdapter(StateAdapter);
+    }
+
 }
