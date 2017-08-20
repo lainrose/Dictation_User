388f932beab6c35d1ff6010ab00979cd323a7ee7
lainrose <lainrose@Naver.com>
2017-08-21 03:08:59 +0900

    Painting the wrong word

--- app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/NaverSpellChecker.java
+++ app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/NaverSpellChecker.java
@@ -8,21 +8,23 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 /**
  * Created by user on 2017-08-20.
  */
 
-public class NaverSpellChecker extends AsyncTask<String, Void, String>{
+public class NaverSpellChecker extends AsyncTask<String, Void, ArrayList<String[]>>{
 
     @Override
-    protected String doInBackground(String... params) {
-        String result = parser(params[0]);
+    protected ArrayList<String[]> doInBackground(String... params) {
+        ArrayList<String[]> result = parser(params[0]);
         return result;
     }
 
-    public String parser(String text){
-        String result = null;
+    public ArrayList<String[]> parser(String text){
+        String parseData = null;
+        ArrayList<String[]> result = new ArrayList<String[]>();
 
         try{
             String param = encodeURIComponent(text);
@@ -30,26 +32,48 @@ public class NaverSpellChecker extends AsyncTask<String, Void, String>{
             URL url = new URL("https://m.search.naver.com/p/csearch/dcontent/spellchecker.nhn?_callback="
                                 + "window.__jindo2_callback._spellingCheck_0&q="
                                 + param);
-
             BufferedReader bin = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-
             while ((line = bin.readLine()) != null){
-                result += line;
+                parseData += line;
             }
             bin.close();
-
         }
         catch (Exception e){
             e.printStackTrace();
             Log.d("Net", "NETWORK ERROR");
         }
 
-        result = result.substring(result.indexOf("\"html\":")+8, result.length()-6);
+        parseData = parseData.substring(parseData.indexOf("\"html\":")+8, parseData.length()-6);
+        String[] temps = parseData.split("</span>");
+        for(String temp : temps){
+            temp = temp.trim();
+            String[] colorWord = new String[2];
+            String[] blackWord = temp.split("(<span class='re_green'>)|(<span class='re_red'>)|(<span class='re_purple'>)");
+
+            if(!blackWord[0].equals("")){
+                result.add(new String[] {"black", blackWord[0]});
+            }
+            if(temp.contains("re_green")){
+                colorWord[0] = "green";
+                colorWord[1] = temp.split(">")[1].trim();
+            }
+            else if(temp.contains("re_red")){
+                colorWord[0] = "red";
+                colorWord[1] = temp.split(">")[1].trim();
+            }
+            else if(temp.contains("re_purple")){
+                colorWord[0] = "purple";
+                colorWord[1] = temp.split(">")[1].trim();
+            }
+            if(colorWord[0] != null){
+                result.add(colorWord);
+            }
+        }
         return result;
     }
 
     public static String encodeURIComponent(String s){
-        String result = null;
+        String result;
         try{
             result = URLEncoder.encode(s, "UTF-8").replaceAll("\\+", "%20").replaceAll("\\%21", "!")
                                                     .replaceAll("\\%27", "'").replaceAll("\\%28", "(")
--- app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/ui/main/ExamResultDetailedPage.java
+++ app/src/main/java/com/cbnu/sweng/randombox/dictation_user/dictation_user/ui/main/ExamResultDetailedPage.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.cbnu.sweng.randombox.dictation_user.dictation_user.NaverSpellChecker;
 import com.cbnu.sweng.randombox.dictation_user.dictation_user.R;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class ExamResultDetailedPage extends AppCompatActivity {
@@ -18,14 +19,17 @@ public class ExamResultDetailedPage extends AppCompatActivity {
 
         NaverSpellChecker naverSpellCheckParser = new NaverSpellChecker();
 
-        String result = null;
+        ArrayList<String[]> results;
         try {
-            result = naverSpellCheckParser.execute("아버지가방에드러가쉰다. 막춤법 성공!").get();
+            results = naverSpellCheckParser.execute("맞춤법검사기테스트.. 입니다. 무라고헤야 테슽트를 제대로 진헹할 수 있지? 맞춤법어케틀림 아니표준어 의심단어말고 막춤법 성공!").get();
+            for(String[] result : results){
+                Log.d("NAVER", "color : " + result[0] + " word : " + result[1]);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        Log.d("NAVER", result);
+
     }
 }
